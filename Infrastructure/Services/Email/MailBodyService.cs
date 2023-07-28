999954022f10e555a0f10db98863259508facdcb
Kevin Shemili <kevinshemili5@gmail.com>
2023-07-28 11:48:10 +0200

    Dynamic url path when sending emails

--- Infrastructure/Services/Email/MailBodyService.cs
+++ Infrastructure/Services/Email/MailBodyService.cs
@@ -1,16 +1,21 @@
 ﻿using Application.Interfaces.Email;
+using Microsoft.AspNetCore.Http;
 
 namespace Application.Services {
     public class MailBodyService : IMailBodyService {
 
-        private const string URL = "https://localhost:58438";
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public MailBodyService(IHttpContextAccessor contextAccessor) {
+            _contextAccessor = contextAccessor;
+        }
 
         public async Task<string> GetVerificationMailBodyAsync(string email, string token) {
 
             var path = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "VerifyEmailTemplate.html");
             string htmlTemplate = await File.ReadAllTextAsync(path);
 
-            var body = htmlTemplate.Replace("ReplaceMe", $"{URL}/auth/verify-email?token={token}&email={email}");
+            var body = htmlTemplate.Replace("ReplaceMe", $"{GetUrl()}/auth/verify-email?token={token}&email={email}");
 
             return body;
         }
@@ -30,7 +35,7 @@ namespace Application.Services {
             var path = Path.Combine(Directory.GetCurrentDirectory(), "Templates", "ForgotPasswordTemplate.html");
             string htmlTemplate = await File.ReadAllTextAsync(path);
 
-            var body = htmlTemplate.Replace("ReplaceMe", $"{URL}/auth/reset-password?token={token}&email={email}");
+            var body = htmlTemplate.Replace("ReplaceMe", $"{GetUrl()}/auth/reset-password?token={token}&email={email}");
 
             return body;
         }
@@ -41,6 +46,12 @@ namespace Application.Services {
             string body = await File.ReadAllTextAsync(path);
 
             return body;
+        }
+
+        private string GetUrl() {
+            var url = _contextAccessor.HttpContext?.Request?.Host.ToString();
+            url = "https://" + url;
+            return url;
         }
     }
 }
